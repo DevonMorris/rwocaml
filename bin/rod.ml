d0a7bdf6a6b561a61740017983ff563d38bc4812
Devon Morris <devonmorris1992@gmail.com>
2023-08-26 21:36:56 -0400

    Truly cursed backtracking code

--- bin/rod.ml
+++ bin/rod.ml
@@ -1,5 +1,9 @@
+open Core
 open Rwo.Rod
 
 let prices = [ 1; 5; 8; 9; 10; 17; 17; 20; 24; 30 ]
-let len = 10
-let () = Printf.printf "%i\n" (profit_rods prices len)
+let len = 8
+let profit, lens = profit_rods prices len
+let () = Printf.printf "Profit: %i\n" profit
+let () = Printf.printf "Rod Lengths:\n"
+let () = List.iter ~f:(fun x -> Printf.printf "%i\n" x) lens
--- lib/rod.ml
+++ lib/rod.ml
@@ -2,19 +2,31 @@ open Core
 
 (** Computes most profitable rods *)
 let profit_rods prices len =
+  let max_by_snd l =
+    l
+    |> List.max_elt ~compare:(fun x y -> compare_int (snd x) (snd y))
+    |> Option.value_exn
+  in
   let new_entry i j v = List.nth_exn prices (i - j) |> ( + ) v in
+  (* tbl here is (len of last rod, total price) *)
   let rec aux tbl =
     let i = List.length tbl in
     if i = len then tbl
     else
       let p = List.nth_exn prices i in
-      let v' =
-        tbl
-        |> List.foldi ~init:Int.min_value ~f:(fun j acc v ->
-               new_entry (i - 1) j v |> max acc)
-        |> max p
+      let tbl' =
+        tbl |> List.mapi ~f:(fun j v -> (i - j, new_entry (i - 1) j (snd v)))
       in
+      let tbl' = (i + 1, p) :: tbl' in
+      let v' = tbl' |> max_by_snd in
       tbl @ [ v' ] |> aux
   in
   let tbl = aux [] in
-  tbl |> List.fold ~init:Int.min_value ~f:max
+  let max_v = tbl |> max_by_snd in
+  (* compute the length of all rods *)
+  let rec backtrack len_remain lens len_take =
+    let len' = len_remain - len_take in
+    if len' = 0 then len_take :: lens
+    else backtrack len' (len_take :: lens) (fst (List.nth_exn tbl (len' - 1)))
+  in
+  (snd max_v, backtrack len [] (fst max_v))
